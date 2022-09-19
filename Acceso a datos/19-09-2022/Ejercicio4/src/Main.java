d1cea5aa647c7c87e0c1c7dede0864c0e7fbbd9a
Lucía Viqueira <luciaviqueira9@gmail.com>
2022-09-19 14:41:42 +0200

    Ejercicio4 BufferedReader y Writter

---
+++ Acceso a datos/19-09-2022/Ejercicio4/src/Main.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        FileReader leerNotas = new FileReader("C:\\Users\\FP\\Documents\\GitHub\\LuciaViqueiraFuentes\\Acceso a datos\\19-09-2022\\Ejercicio4\\notas.txt");
+
+        BufferedReader leer = new BufferedReader(leerNotas);
+
+        String leerFichero = new String(leer.readLine());
+
+        while (leerNotas !=null){
+            
+        }
+
+    }
+}
