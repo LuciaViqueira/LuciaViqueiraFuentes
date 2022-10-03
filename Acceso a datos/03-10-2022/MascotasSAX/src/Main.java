63242b2f69e1cfeed3c87f72305095753197609d
Lucía Viqueira <luciaviqueira9@gmail.com>
2022-10-03 14:38:16 +0200

    SAX

--- Acceso a datos/03-10-2022/MascotasSAX/src/Main.java
+++ Acceso a datos/03-10-2022/MascotasSAX/src/Main.java
@@ -1,4 +1,3 @@
-
 import org.xml.sax.InputSource;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.XMLReaderFactory;
@@ -7,19 +6,19 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-
         try{
             XMLReader lector =  XMLReaderFactory.createXMLReader();
-            ParseadorAlumnosSAX parser = new ParseadorAlumnosSAX();
+            ParseadorMascotasSAX parser = new ParseadorMascotasSAX();
             lector.setContentHandler(parser);
-            InputSource fichero = new InputSource("C:\\Users\\FP\\Documents\\GitHub\\LuciaViqueiraFuentes\\Acceso a datos\\03-10-2022\\LeerXML_SAX\\SAX.xml");
+            InputSource fichero = new InputSource("C:\\Users\\FP\\Documents\\GitHub\\LuciaViqueiraFuentes\\Acceso a datos\\03-10-2022\\Ejercicio2.xml");
             lector.parse(fichero);
-            ArrayList<Alumno> lista = parser.obtenerResultado();
+            ArrayList<Mascota> lista = parser.obtenerResultado();
             System.out.println(lista);
 
         }catch (Exception ex){
             System.out.println("Excepcion en el procesamiento del XLM");
             ex.printStackTrace();
         }
+
     }
 }
---
+++ Acceso a datos/03-10-2022/MascotasSAX/src/ParseadorMascotasSAX.java
@@ -0,0 +1,70 @@
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.util.ArrayList;
+
+public class ParseadorMascotasSAX extends DefaultHandler {
+    private ArrayList<Mascota> listado;
+
+    private Mascota mascota;
+
+    private String valorElemento;
+
+    enum tiposNodo{
+        MASCOTAS,
+        MASCOTA,
+        TIPO,
+        EDAD,
+        GENERO
+    }
+
+    public ParseadorMascotasSAX(){
+        super();
+    }
+
+    @Override
+    public void startDocument() throws SAXException{
+        listado = new ArrayList<Mascota>();
+    }
+
+    public void startElement(String uri, String localName, String qName, Attributes attributes){
+        if(localName!=null){
+            switch (tiposNodo.valueOf(localName.toUpperCase())){
+                case MASCOTA:
+                    mascota = new Mascota();
+                    String nombre = attributes.getValue("nombre");
+                    if(nombre!=null){
+                        mascota.setNombre(nombre);
+                    }
+                    break;
+            }
+        }
+    }
+    @Override
+    public void endElement(String uri, String localName, String qName ) throws SAXException{
+        if (localName!= null){
+            switch (tiposNodo.valueOf(localName.toUpperCase())){
+                case MASCOTA:
+                    listado.add(mascota);
+                    break;
+                case TIPO:
+                    mascota.setNombre(valorElemento) ;
+                    break;
+                case EDAD:
+                    mascota.setEdad(Integer.parseInt(valorElemento));
+                    break;
+            }
+        }
+    }
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException{
+        valorElemento = new String(ch, start, length);
+    }
+    public ArrayList<Mascota> obtenerResultado(){
+        return listado;
+    }
+
+
+
+}
