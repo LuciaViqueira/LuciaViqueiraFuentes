beaf2b9704c198c915763261d702f27b409bf382
Lucía Viqueira <luciaviqueira9@gmail.com>
2022-10-20 12:30:28 +0200

    20/10/2022
    ME falta que cuando termine de pedir termine

--- Acceso a datos/11-10-2022/Tarea1/src/main/java/org/example/Main.java
+++ Acceso a datos/11-10-2022/Tarea1/src/main/java/org/example/Main.java
@@ -4,18 +4,21 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+    static Scanner teclado = new Scanner(System.in);
     public static void main(String[] args) throws JAXBException {
 
         boolean finalpartida = false;
-        int escena = 1;
-        //Aventura aventura = realizarLecturaXML(".\\aventura.xml");
+        int escena = 0;
+        int escoger = 0;
+        Aventura aventura = realizarLecturaXML(".\\aventura.xml");
         //System.out.println(aventura);
-        leerAventura();
-
-
+        System.out.println(aventura.getTitulo());
+        while(!finalpartida){
+            escena = leerAventura(aventura, escena, finalpartida, escoger);
+        }
 
     }
     private static Aventura realizarLecturaXML (String ruta) throws JAXBException{
@@ -33,14 +36,23 @@ public class Main {
         return aventura;
     }
 
-    private static void leerAventura() throws JAXBException {
-        Aventura aventura = realizarLecturaXML(".\\aventura.xml");
-        System.out.println(aventura.getTitulo());
-        System.out.println(aventura.getEscenas());
+    private static int leerAventura(Aventura aventura, int escena, boolean finalPartida, int escoger) {
 
 
+        System.out.println(aventura.getEscenas().get(escena).getTexto());
+        if(finalPartida == false){
+            for(int i = 0; i<aventura.getEscenas().get(escena).getOpciones().size(); i++){
+                System.out.println(aventura.getEscenas().get(escena).getOpciones().get(i).getTexto());
 
+            }
+            escoger = teclado.nextInt();
+            
+            escena =  aventura.getEscenas().get(escena).getOpciones().get(escoger - 1).getResultado();
+        }
+
+        return escena;
 
     }
+
 
 }
