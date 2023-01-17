7a17427783b3344951149ce3d40c3623399534a3
Lucía Viqueira <luciaviqueira9@gmail.com>
2023-01-17 14:28:12 +0100

    Ejercicio

--- Acceso a datos/EjercicioJDBC/Tarea/src/main/java/org/example/App.java
+++ Acceso a datos/EjercicioJDBC/Tarea/src/main/java/org/example/App.java
@@ -14,9 +14,11 @@ import java.sql.SQLException;
  */
 public class App 
 {
-    public static void main( String[] args ) throws JAXBException {
+    public static void main( String[] args ) throws JAXBException, SQLException {
 
          Location l = leerXML(".\\es.xml");
+       //  insertarBDUnoaUno(l);
+        insertarLote(l);
     }
     private  static Location leerXML(String ruta) throws JAXBException {
 
@@ -32,25 +34,36 @@ public class App
             System.out.println("No se ha podido encontrar el fichero indicado");
         }
         for (int i = 0; i<location.getCountryRegion().size(); i++){
-            System.out.println(location.getCountryRegion().get(i));
+           System.out.println(location.getCountryRegion().get(i));
         }
         return location;
     }
 
     public static void insertarBDUnoaUno(Location location) throws SQLException {
-        //Recorrer con while o for
-
-        //Insertamos Ciudad
         Connection c = ConexionHikari.obtenerConexion();
         PreparedStatement s = null;
-
+        long inicio = System.currentTimeMillis();
         try {
             s = c.prepareStatement("INSERT INTO Ciudades (codigo, nombre) VALUES (?,?)");
 
-            //Para que mande los datos a la base
-            s.executeUpdate();
-            System.out.println("Insertado ciudad Ok");
+            for (CountryRegion contry: location.getCountryRegion()) {
+                if (contry.getState() != null) {
+                    for (State state: contry.getState()) {
+                        if(state.getCity()!=null){
+                            for (City city: state.getCity()) {
+                                s.setString(1, city.getCode());
+                                s.setString(2, city.getName());
+                                s.executeUpdate();
+                            }
+                        }
 
+                    }
+                }
+                }
+
+            System.out.println("Insertado ciudad Ok");
+            long tiempo = System.currentTimeMillis() - inicio ;
+            System.out.println(tiempo);
         }catch (SQLException e){
             System.out.println("Insertado ciudad KO");
             throw new RuntimeException(e);
@@ -61,7 +74,38 @@ public class App
 
 
     }
-    public static void insertarLote(Location location){
+    public static void insertarLote(Location location) throws SQLException {
+        Connection c = ConexionHikari.obtenerConexion();
+        PreparedStatement s = null;
+        long inicio = System.currentTimeMillis();
+        try {
+            s = c.prepareStatement("INSERT INTO Ciudades (codigo, nombre) VALUES (?,?)");
+
+            for (CountryRegion contry: location.getCountryRegion()) {
+                if (contry.getState() != null) {
+                    for (State state: contry.getState()) {
+                        if(state.getCity()!=null){
+                            for (City city: state.getCity()) {
+                                s.setString(1, city.getCode());
+                                s.setString(2, city.getName());
+                                s.addBatch();
+                            }
+                        }
+
+                    }
+                }
+            }
+            s.executeBatch();
+            System.out.println("Insertado lote Ok");
+            long tiempo = System.currentTimeMillis() - inicio ;
+            System.out.println(tiempo);
+        }catch (SQLException e){
+            System.out.println("Insertado lote KO");
+            throw new RuntimeException(e);
+        }finally {
+            if(s!=null) s.close();
+            if(c!=null) c.close();
+        }
 
     }
 }
