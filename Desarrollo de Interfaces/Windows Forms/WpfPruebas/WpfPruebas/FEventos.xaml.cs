03778755d1adb9b6484b929167fdd6ddeaa72716
Lucía Viqueira <luciaviqueira9@gmail.com>
2023-01-19 14:34:55 +0100

    19/01/2023

---
+++ Desarrollo de Interfaces/Windows Forms/WpfPruebas/WpfPruebas/FEventos.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfPruebas
+{
+    /// <summary>
+    /// Lógica de interacción para FEventos.xaml
+    /// </summary>
+    public partial class FEventos : Window
+    {
+        NombrePersona persona = new NombrePersona { Nombre ="Lucia", Edad= 22};
+        public FEventos()
+        {
+            InitializeComponent();
+            this.DataContext = persona;
+        }
+
+        private void evtSaludar(object sender, RoutedEventArgs e) {
+            MessageBox.Show("Hola" + persona.Nombre, "Saludo",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+
+    }
+
+    
+}
